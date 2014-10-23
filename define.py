4ec1974785c3bff7e779ac08eea0d589b8f69325
Jim Holmström <jim.holmstroem@gmail.com>
2014-10-23 09:35:47 +0200

    Adds caching (both for later memorandom of the word, and faster access)

--- define.py
+++ define.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function, division
 
+from functools import wraps
 import argparse
+import os
 from urllib2 import urlopen, Request
 
 import gtk
@@ -10,32 +12,81 @@ import webkit
 import bs4
 
 
-def main(words):
+def main(
+    words,
+    cache_directory=os.path.join(
+        os.path.expanduser('~'),
+        '.define_cache',
+    )
+):
+
+    def concatenate_words(words):
+        return '+'.join(words)
+
+    def cached(get_data):
+        @wraps(get_data)
+        def _cached(words):
+            word = concatenate_words(words)
+            cache_name = os.path.join(
+                cache_directory,
+                '{word}.htm'.format(word=word),
+            )
+
+            def save(data, word, cache_directory=cache_directory):
+                try:
+                    os.mkdir(cache_directory)
+                except OSError as ose:
+                    ose
+
+                with open(cache_name, 'w') as f:
+                    map(f.write, data)
+
+                return data
+
+            if os.path.isfile(cache_name):
+                with open(cache_name) as f:
+                    return (
+                        '<div style='
+                        '"position: fixed; bottom: 4px; right: 4px;">'
+                        '[CACHED]'
+                        '</div>'
+                    ) + "\n".join(f)
+            else:
+                return save(
+                    data=get_data(words),
+                    word=word
+                )
+
+        return _cached
+
     def address(words):
         return "http://{subdomain}thefreedictionary.com/{word}".format(
             subdomain="idioms." if len(words) > 1 else "",
-            word='+'.join(words)
+            word=concatenate_words(words)
         )
 
-    data, = map(str, bs4.BeautifulSoup(
-        urlopen(
-            Request(
-                url=address(
-                    words
-                ),
-                headers={
-                    'User-Agent': (
-                        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:33.0) '
-                        'Gecko/20100101 Firefox/33.0'
+    data = cached(
+        lambda words: str(bs4.BeautifulSoup(
+            urlopen(
+                Request(
+                    url=address(
+                        words
                     ),
-                }
+                    headers={
+                        'User-Agent': (
+                            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:33.0) '
+                            'Gecko/20100101 Firefox/33.0'
+                        ),
+                    }
+                )
             )
-        )
-    ).find_all('div', id='MainTxt'))
+        ).find_all('div', id='MainTxt')[0])
+    )(words)
 
     window = gtk.Window()
     window.connect("destroy", gtk.mainquit)
     webview = webkit.WebView()
+
     webview.load_html_string(data, "http://thefreedictionary.com")
     window.add(webview)
     window.show_all()
